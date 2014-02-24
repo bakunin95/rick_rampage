7b4db99ee334a6012e19b7917ba6855bed2321b2
albertodotcom <alberto.forn@gmail.com>
2014-02-24 12:14:27 +1100

    Rails scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RickRampage::Application.config.secret_key_base = '533a128473c196577f6abbfd8067eb1dc2cc3b309ae158723163aa0b7d35d9c29a6676ffe732a83f403a3b1f9b07e6a2067b1eac22aab041e490bde8997099f0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RickRampage::Application.config.session_store :cookie_store, key: '_rick_rampage_session'
